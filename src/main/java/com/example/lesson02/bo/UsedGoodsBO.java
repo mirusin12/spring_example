b87df7a2ab97d73474a29b3ae5d5f79b851a7a20
DESKTOP-UF7MBHT\82103 <test@test.com>
2024-03-24 17:52:16 +0900

    3/22 강의

---
+++ src/main/java/com/example/lesson02/bo/UsedGoodsBO.java
@@ -0,0 +1,11 @@
+package com.example.lesson02.bo;
+
+import org.springframework.stereotype.Service;
+
+@Service // Spring bean
+public class UsedGoodsBO {
+	
+	// input: (컨트롤러로부터 받아온다.)  X
+	// output: (컨트롤러한테 준다.) List<UsedGoods>
+	
+}
