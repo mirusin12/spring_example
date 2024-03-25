4b50292126ca44c641c3a142a11946ffe3516596
DESKTOP-UF7MBHT\82103 <test@test.com>
2024-03-25 16:50:23 +0900

    3/22 강의

--- src/main/java/com/example/lesson02/bo/UsedGoodsBO.java
+++ src/main/java/com/example/lesson02/bo/UsedGoodsBO.java
@@ -1,11 +1,24 @@
 package com.example.lesson02.bo;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.lesson02.domain.UsedGoods;
+import com.example.lesson02.mapper.UsedGoodsMapper;
 
 @Service // Spring bean
 public class UsedGoodsBO {
 	
+	@Autowired // Dependency Injection(DI): 의존성 주입**
+	private UsedGoodsMapper usedGoodsMapper; // Spring bean을 가져온다.
+	
+	
 	// input: (컨트롤러로부터 받아온다.)  X
 	// output: (컨트롤러한테 준다.) List<UsedGoods>
+	public List<UsedGoods> getUsedGoodsList() {
+		return usedGoodsMapper.selectUsedGoodsList();
+	}
 	
 }
